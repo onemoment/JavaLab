832930b69d471bdd60135ad0a3f1c15632028579
student <student>
2015-12-05 14:31:25 +0100

    zadanie 2

---
+++ JLab01_Zad02/src/Zadanie02.java
@@ -0,0 +1,15 @@
+
+public class Zadanie02 {
+
+	public static void main(String[] args) {
+	    
+	    for (int i=1; i<=12; i++){
+	    	System.out.print(i + "\t");
+	    	if (i % 3 == 0){
+	    		System.out.print("\n");
+	    	}
+	    }
+	    //
+	}
+
+}
